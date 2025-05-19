fa296d87237feef65b399d159346b878773c5443
patcheco2025 <melocavalcante0311@gmail.com>
2025-05-19 14:03:50 -0300

    Update App.js
    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>Bem-vindo à PatchEco!</h1>
-    </div>
-  );
+  const [mostrarSegundaTela, setMostrarSegundaTela] = useState(false);
+
+  const irParaSegundaTela = () => {
+    setMostrarSegundaTela(true);
+  };
+
+  return (
+    <div>
+      {mostrarSegundaTela ? (
+        <h2>Esta é a segunda tela!</h2>
+      ) : (
+        <div>
+          <h1>Bem-vindo à PatchEco!</h1>
+          <button onClick={irParaSegundaTela}>Entrar</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
